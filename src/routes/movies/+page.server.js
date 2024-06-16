459524bdbd770b5de4ec5ccc7b9b2505f0d05eca
Fouche du Preez <fouche@limehouse.co.za>
2024-06-16 22:50:35 +0200

    loading db data

--- src/routes/movies/+page.server.js
+++ src/routes/movies/+page.server.js
@@ -1,23 +1,11 @@
-export function load() {
+export async function load() {
 
-	const moviesList = [
-		{
-			id: 1,
-			title: 'Highlander',
-			release_date: '1986-03-07',
-			runtime: 116,
-			mpaa_rating: 'R',
-			description: 'Some long description'
-		},
-		{
-			id: 2,
-			title: 'Raiders of the lost Ark',
-			release_date: '1981-06-12',
-			runtime: 115,
-			mpaa_rating: 'PG-13',
-			description: 'Some long description'
-		}
-	];
+	let moviesList = []
+
+	await fetch(`http://localhost:9080/movies`)
+		.then((res) => res.json())
+		.then(data => moviesList = data)
+		.catch(err => console.log(err));
 
 	return { moviesList };
 }
--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,7 +1,12 @@
 <script>
-	export let data;
-</script>
+	export let data
 
+	// async function getMovies() {
+	// 	return fetch(`http://localhost:9080/movies`)
+	// 			.then((res) => res.json())
+	// 			.catch(err => console.log(err));
+	// }
+</script>
 <div>
 	<h2>Movies</h2>
 	<hr />
@@ -14,10 +19,21 @@
 		</tr>
 		</thead>
 		<tbody>
+		<!--{#await getMovies()}-->
+		<!--	<tr><td colspan="3" class="text-center">Loading...</td></tr>-->
+		<!--{:then movies}-->
+		<!--	{#each movies as movie}-->
+		<!--		<tr>-->
+		<!--			<td><a href="/movies/{movie.id}">{movie.title}</a></td>-->
+		<!--			<td>{movie.release_date}</td>-->
+		<!--			<td>{movie.mpaa_rating}</td>-->
+		<!--		</tr>-->
+		<!--	{/each}-->
+		<!--{/await}-->
 		{#each data.moviesList as movie}
 			<tr>
 				<td><a href="/movies/{movie.id}">{movie.title}</a></td>
-				<td>{movie.release_date}</td>
+				<td>{movie.release_date.substring(0,10)}</td>
 				<td>{movie.mpaa_rating}</td>
 			</tr>
 		{/each}
